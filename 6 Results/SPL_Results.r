84108cfc5f19fac4b8db9d7fe69586b169ba038c
marlaks <mark_lakshman@web.de>
2017-08-06 20:39:42 +0200

    Update SPL_Results.r

--- 6 Results/SPL_Results.r
+++ 6 Results/SPL_Results.r
@@ -6,19 +6,22 @@ accuracy.results = data.frame(lr.results.accuracy, svm.results.accuracy, dt.resu
 ggplot(accuracy.results, aes(x = iteration)) + geom_line(aes(y = dt.results.accuracy, colour = "dt.results.accuracy")) + 
     geom_line(aes(y = lr.results.accuracy, colour = "lr.results.Accuracy")) + geom_line(aes(y = svm.results.accuracy, 
     colour = "svm.results.accuracy")) + geom_line(aes(y = nnt.results.accuracy, colour = "nnt.results.accuracy")) + 
-    ylab("accuracy percentage") + ggtitle("Accuracy of each model over multiple iterations") + theme(panel.background = element_rect(fill = "transparent"))
+    ylab("accuracy percentage") + ggtitle("Accuracy of each model over multiple iterations") + 
+    theme(panel.background = element_rect(fill = "transparent"))
 precision.results = data.frame(lr.results.precision, svm.results.precision, dt.results.precision, 
     nnt.results.precision, iteration = c(1:10))
 ggplot(precision.results, aes(x = iteration)) + geom_line(aes(y = dt.results.precision, colour = "dt.results.precision")) + 
     geom_line(aes(y = lr.results.precision, colour = "lr.results.precision")) + geom_line(aes(y = svm.results.precision, 
     colour = "svm.results.precision")) + geom_line(aes(y = nnt.results.precision, colour = "nnt.results.precision")) + 
-    ylab("precision") + ggtitle("precision of each model over multiple iterations") + theme(panel.background = element_rect(fill = "transparent"))
+    ylab("precision") + ggtitle("precision of each model over multiple iterations") + 
+    theme(panel.background = element_rect(fill = "transparent"))
 recall.results = data.frame(lr.results.recall, svm.results.recall, dt.results.recall, nnt.results.recall, 
     iteration = c(1:10))
 ggplot(recall.results, aes(x = iteration)) + geom_line(aes(y = dt.results.recall, colour = "dt.results.recall")) + 
     geom_line(aes(y = lr.results.recall, colour = "lr.results.recall")) + geom_line(aes(y = svm.results.recall, 
     colour = "svm.results.recall")) + geom_line(aes(y = nnt.results.recall, colour = "nnt.results.recall")) + 
-    ylab("recall") + ggtitle("recall of each model over multiple iterations") + theme(panel.background = element_rect(fill = "transparent"))
+    ylab("recall") + ggtitle("recall of each model over multiple iterations") + 
+    theme(panel.background = element_rect(fill = "transparent"))
 
 # calculate mean and sd from recall and precision and compare results in table
 recall.means    = lapply(recall.results[1:4], FUN = mean)
